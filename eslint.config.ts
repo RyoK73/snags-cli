6b0a984a4a305a43400b33c0a4dcd72cd6dc2078
RyoK73 <74401068+CodingRajaF@users.noreply.github.com>
2026-09-07 16:59:39 +0900

    doc: Add the fundamental files
    Why split tsconfig.json
    - To split commands for test and build.
    - On test(check), tsc targets all the `.ts` files.
    - On build, tsc targets the `src/**/*.ts` files.
    
    What
    - Add 2 tsconfig.json files.
    - Add ESLint and a config file.
    - Add Vitest and a config file.
    - Fill out the required field in package.json.

---
+++ eslint.config.ts
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist/**"]),
+  tseslint.configs.strictTypeChecked,
+  {
+    plugins: { js },
+    extends: ["js/recommended"],
+    files: ["**/*.{ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ["**/*.test.ts"],
+    languageOptions: { globals: { ...globals.node, ...globals.vitest } },
+  },
+]);
